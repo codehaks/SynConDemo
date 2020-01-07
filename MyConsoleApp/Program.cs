746c9b3410e9db4c3be891cd25c6c0d77a25e36e
Hakim Ghods <ghods.hakim@hotmail.com>
2020-01-07 05:58:16 +0330

    fix

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -11,27 +11,44 @@ namespace MyConsoleApp
         {
             Console.WriteLine("Hello World!");
 
-            System.Diagnostics.Trace.WriteLine($"Before delay ...{Thread.CurrentThread.ManagedThreadId}");
-            await DoAsync();
-            System.Diagnostics.Trace.WriteLine($"After delay ...{Thread.CurrentThread.ManagedThreadId}");
+            System.Diagnostics.Trace.WriteLine($"Before DoAsync...{Thread.CurrentThread.ManagedThreadId}");
+            await DoAsync().ConfigureAwait(true);
+            System.Diagnostics.Trace.WriteLine($"After DoAsync ...{Thread.CurrentThread.ManagedThreadId}");
 
             Console.WriteLine("Done!");
-
+            System.Diagnostics.Trace.WriteLine($" Done ...{Thread.CurrentThread.ManagedThreadId}");
         }
 
         private static async Task DoAsync()
         {
-            
+
 
             System.Diagnostics.Trace.WriteLine($"DoAsync start ...{Thread.CurrentThread.ManagedThreadId}");
-            var clinet = new HttpClient();
-            //await clinet.GetAsync("https://codehaks.com");
-            await Task.Delay(2000);
+
+            await DownLoad();
+
+            System.Diagnostics.Trace.WriteLine($"DoAsync downloaded ...{Thread.CurrentThread.ManagedThreadId}");
+            var t1 = Thread.CurrentThread.ManagedThreadId;
             await Task.Run(() =>
             {
                 System.Diagnostics.Trace.WriteLine($"DoAsync run ...{Thread.CurrentThread.ManagedThreadId}");
+                var t2 = Thread.CurrentThread.ManagedThreadId;
+
+                System.Diagnostics.Trace.WriteLine($"Jumped from {t1} to {t2}");
             });
+            
             System.Diagnostics.Trace.WriteLine($"DoAsync end ...{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private static async Task DownLoad()
+        {
+            var t1 = Thread.CurrentThread.ManagedThreadId;
+            var clinet = new HttpClient();
+            await clinet.GetAsync("https://codehaks.com");
+            var t2 = Thread.CurrentThread.ManagedThreadId;
+            //await Task.Delay(1000);
+
+            System.Diagnostics.Trace.WriteLine($"Jumped from {t1} to {t2}");
         }
     }
 }
